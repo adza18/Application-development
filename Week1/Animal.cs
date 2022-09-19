1adf2cf62954a512e1c146ebe6129e934808b1df
adza <adrizaacharya1@gmail.com>
2022-09-19 09:17:33 +0545

    first commit

---
+++ Week1/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    public class Animal
+    {
+        string name;
+        int age;
+        string color;
+
+        public Animal()
+        {
+            this.name = "Dog";
+            this.age = 12;
+            this.color = "Brown";
+        }
+
+        public string getAnimal()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ Week1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Week1;
+
+Console.WriteLine("Enter your name");
+string s = Console.ReadLine();
+Console.WriteLine("Welcome!" + s);
+Console.WriteLine("Calling method of another class");
+
+Week1.Animal a = new Animal();
+a.getAnimal();
+Console.WriteLine("Value returned by method of Animal class " + a.getAnimal());
+
+
+
